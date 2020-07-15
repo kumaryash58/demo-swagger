952c0448d7aec134b3028e14dc226bbcbff80751
kumaryash58 <40853208+kumaryash58@users.noreply.github.com>
2020-07-15 14:24:16 +0530

    Update SwaggerDemoApplication.java
    comment

--- SwaggerDemo/src/main/java/com/techment/SwaggerDemo/SwaggerDemoApplication.java
+++ SwaggerDemo/src/main/java/com/techment/SwaggerDemo/SwaggerDemoApplication.java
@@ -31,7 +31,7 @@ public class SwaggerDemoApplication extends SpringBootServletInitializer{
 	
 
 	
-	//Test
+	//Test-----------
 	//@TestPropertySource(locations = "classpath:application.properties")
 	
 	public static void main(String[] args) {
