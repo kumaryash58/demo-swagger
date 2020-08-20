44e3524c7645a31814f527c9e990c96a286cd132
ykDewangan <kumaryashaswi58@gmail.com>
2020-08-20 18:48:59 +0530

    calc comment

--- SwaggerDemo/src/main/java/com/techment/SwaggerDemo/MyCalc.java
+++ SwaggerDemo/src/main/java/com/techment/SwaggerDemo/MyCalc.java
@@ -8,7 +8,7 @@ class MyCalc extends Frame
 public boolean setClear=true;
 double number, memValue;
 char op;
-
+//demo calc
 String digitButtonText[] = {"7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "+/-", "." };
 String operatorButtonText[] = {"/", "sqrt", "*", "%", "-", "1/X", "+", "=" };
 String memoryButtonText[] = {"MC", "MR", "MS", "M+" };
